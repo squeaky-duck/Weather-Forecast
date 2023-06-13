f360b68692af10b5fbbda7714af05cfc3d8a3afb
sqeaky-duck <mehulj35@gmail.com>
2023-06-14 00:58:31 +0530

    Add backend code to fetch weather data

---
+++ backend.py
@@ -0,0 +1,18 @@
+import requests
+import os
+API_KEY = os.getenv("WEATHER_API_KEY")
+
+
+def get_data(place, forecast_days):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+
+    return filtered_data
+
+
+if __name__ == "__main__":
+    print(get_data(place="tokyo", forecast_days=3))
